a63af0cc154a2ff40f7be71f39dbd99ad2345560
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-02-13 20:05:19 -0500

    Add files via upload

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,2 +1,23 @@
-/* 0.5.0 2022-5-20 0:0:0 */
-var app=new function(){this.name="Two Faces ",this.version="9",this.date="2022",this.folder="asset-v1/",this.looptime=5640,this.bpm=85,this.totalframe=246,this.nbpolo=7,this.nbloopbonus=4,this.bonusloopA=!0,this.bonusendloopA=!4,this.recmaxloop=34,this.recminloop=8,this.recmintime=Math.round(this.looptime/1e3)*this.recminloop,this.spritepolo="polo-sprite.png",this.spritepicto="game-picto.png",this.colBck="#112221",this.col0="#AAE0B5",this.col1="#77B685",this.col2="#469657",this.col3="#296836",this.col4="#0C3925",this.animearray=[{name:"1_conductor",color:"007C87",uniqsnd:!0},{name:"2_marvin",color:"007C87",uniqsnd:!0},{name:"3_cloky",color:"007C87",uniqsnd:!1},{name:"4_shoebill",color:"007C87",uniqsnd:!0},{name:"5_oxygen",color:"007C87",uniqsnd:!1},{name:"6_oscar",color:"8F023D",uniqsnd:!1},{name:"7_detector",color:"8F023D",uniqsnd:!1},{name:"8_reffrey",color:"8F023D",uniqsnd:!0},{name:"9_flower",color:"8F023D",uniqsnd:!1},{name:"10_velda",color:"8F023D",uniqsnd:!1},{name:"11_major",color:"F8BC4D",uniqsnd:!0},{name:"12_pipes",color:"F8BC4D",uniqsnd:!1},{name:"13_omega",color:"F8BC4D",uniqsnd:!1},{name:"14_glaucoma",color:"F8BC4D",uniqsnd:!1},{name:"15_neuralgia",color:"F8BC4D",uniqsnd:!1},{name:"16_guard",color:"479758",uniqsnd:!1},{name:"17_magician",color:"479758",uniqsnd:!1},{name:"18_choir",color:"479758",uniqsnd:!1},{name:"19_faces",color:"479758",uniqsnd:!1},{name:"20_future",color:"479758",uniqsnd:!1}],this.bonusarray=[{name:"Magic",src:"v1-b1-magic-hb.mp4",code:"1,3,4,11,17",sound:"bonus-magic",aspire:"aspire"}];for(var n=0,o=this.animearray.length;n<o;n++){var a=this.animearray[n].name;this.animearray[n].soundA=a+"_a",this.animearray[n].soundB=this.animearray[n].uniqsnd?a+"_a":a+"_b",this.animearray[n].anime=a+"-sprite.png",this.animearray[n].animeData=a+".json"}};
+/** hi **/
+var app=new function(){this.name="Wolfgang",this.version="1",this.date="2024",this.folder="asset-v1/",this.looptime=6350,this.bpm=151,this.totalframe=304,this.nbpolo=7,this.nbloopbonus=[4],this.bonusloopA=!0,this.bonusendloopA=!0,this.recmaxloop=27,this.recminloop=2,this.recmintime=Math.round(this.looptime/1e3)*this.recminloop,this.spritepolo="polo-sprite.png",this.spritepicto="game-picto.png",this.colBck="#000000",this.col0="#000000",this.col1="#d9be00",this.col2="#000000",this.col3="#6e2e00",this.col4="#ffffff",this.animearray=[
+{name:"01_scout",color:"82499e",uniqsnd:!0},
+{name:"02_clippor",color:"82499e",uniqsnd:!0},
+{name:"03_cl4ym0r3",color:"82499e",uniqsnd:!0},
+{name:"04_visarus",color:"82499e",uniqsnd:!0},
+{name:"05_dewie",color:"82499e",uniqsnd:!1},
+{name:"06_roar",color:"DC2A4A",uniqsnd:!0},
+{name:"07_bluerose",color:"DC2A4A",uniqsnd:!0},
+{name:"08_lover",color:"DC2A4A",uniqsnd:!0},
+{name:"09_barru",color:"DC2A4A",uniqsnd:!0},
+{name:"10_bazardg",color:"DC2A4A",uniqsnd:!1},
+{name:"11_rick",color:"FFD52B",uniqsnd:!1},
+{name:"12_freaker",color:"FFD52B",uniqsnd:!0},
+{name:"13_riser",color:"FFD52B",uniqsnd:!1},
+{name:"14_sasha",color:"FFD52B",uniqsnd:!0},
+{name:"15_cubix",color:"FFD52B",uniqsnd:!1},
+{name:"16_jake",color:"0A8AC6",uniqsnd:!1},
+{name:"17_drdoctor",color:"0A8AC6",uniqsnd:!0},
+{name:"18_yori",color:"0A8AC6",uniqsnd:!1},
+{name:"19_resonance",color:"0A8AC6",uniqsnd:!1},
+{name:"20_ido",color:"0A8AC6",uniqsnd:!0},
+],this.bonusarray=[{name:"Gray World",src:"b1-gray_world-hb.mp4",code:"2,8,10,12,13",sound:"bonus-gray_world",aspire:"aspire-gray_world"}],this.unlockerarray=[];for(var n=0,o=this.animearray.length;n<o;n++){var a=this.animearray[n].name;this.animearray[n].soundA=a+"_a",this.animearray[n].soundB=this.animearray[n].uniqsnd?a+"_a":a+"_b",this.animearray[n].anime=a+"-sprite.png",this.animearray[n].animeData=a+".json"}};
